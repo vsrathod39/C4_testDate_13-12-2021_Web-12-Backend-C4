5834ad4cc9cc58400e9cbe57310b122d638ab74f
Vikas Kumar <vsrathod39@gmail.com>
2021-12-13 14:44:18 +0530

    mongodb connectiong

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const connect = require("./config/db");
+
+const app = express();
+app.use(express.json());
+
+app.listen(2345, async () => {
+    await connect();
+    console.log("Listening at port 2343...");
+})
